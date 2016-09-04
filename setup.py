8800b1af7e23a7ffaa0d56249603ff9c1403c964
pi <shatki@mail.ru>
2016-09-05 00:15:02 +0300

    create dist

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 pyfastsqrt_module = Extension(name='pyfastsqrt', sources=['pyfastsqrt.c'])
-setup(name='pyfastsqrt', ext_modules=[pyfastsqrt_module])
+setup(name='pyfastsqrt',
+      author='Seliverstov Dmitriy 2016 shatki@mail.ru',
+      version='1.0',
+      ext_modules=[pyfastsqrt_module])
